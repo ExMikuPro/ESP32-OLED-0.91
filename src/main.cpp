9faaefafbd09e0ca198323fe46b72e227c862a63
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-12-25 06:25:08 +0800

    docs: 添加部分注释

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
-
 #include <arduino.h>
 #include "U8g2lib.h"
 
-// U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);  //M0/ESP32/ESP8266/mega2560/Uno/Leonardo
-U8G2_SSD1327_WS_96X64_F_HW_I2C u8g2(U8G2_R0, /* reset=*/
-                                    U8X8_PIN_NONE);
+/* ========================= 接线定义 ========================
+ * SDA -> GPIO21
+ * SCK -> GPIO22
+========================================================== */
+
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE); // SSD1315 0.91小长屏
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
+
     u8g2.begin();
+
     u8g2.setFont(u8g2_font_t0_11_t_all);
     u8g2.drawBox(0, 0, 128, 32);
     u8g2.sendBuffer();
